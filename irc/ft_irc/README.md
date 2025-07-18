0dac8c1a67563ae90c229c62b76a5d4f15f47e56
chrysalxm <156919554+fkuruthl@users.noreply.github.com>
2025-07-19 00:34:05 +0400

    Add files via upload

---
+++ irc/ft_irc/README.md
@@ -0,0 +1,2 @@
+# ft_irc
+ft_irc is a C++98 project to build a functional IRC server that handles multiple clients, supports standard IRC commands, and uses non-blocking I/O with poll().
---
+++ irc/main.cpp
@@ -0,0 +1,23 @@
+#include "Server.hpp"
+
+
+int main(int argc, char **argv) 
+{
+    if (argc != 3) 
+    {
+        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    std::string password = argv[2];
+
+    try {
+        Server server(port, password);
+        server.start();
+    } catch (const std::exception &e) {
+        std::cerr << "Server error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
